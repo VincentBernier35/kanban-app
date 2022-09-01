35df2dc62c049c0e378f4398c5ae3f13e34d35fd
VincentBernier35 <vincent.bernier@hotmail.fr>
2022-09-01 11:19:24 +0200

    🚧 start and test server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
+
+// start the app
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
