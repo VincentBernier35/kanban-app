4c256f0c24c3dd701cbc77ab16c9389c3fd222a6
VincentBernier35 <vincent.bernier@hotmail.fr>
2022-09-01 15:58:37 +0200

    🚧 made all lists routes + start controller + insomnia check OK

--- app/controllers/listController.js
+++ app/controllers/listController.js
@@ -4,6 +4,26 @@ function getAllLists(req, res) {
   res.send("Bienvenue dans getAllLists");
 }
 
+function getOneList(req, res) {
+  res.send("bienvenue dans getOneList");
+}
+
+function createList(req, res) {
+  console.log("Hello from createList");
+}
+
+function updateList(req, res) {
+  console.log("Hello from updateList");
+}
+
+function deleteList(req, res) {
+  console.log("Hello from deleteList");
+}
+
 module.exports = {
   getAllLists,
+  getOneList,
+  createList,
+  updateList,
+  deleteList
 };
--- app/routers/listRouter.js
+++ app/routers/listRouter.js
@@ -6,6 +6,10 @@ const router = new Router();
 
 
 router.get("/", listController.getAllLists);
+router.get("/:id", listController.getOneList);
+router.post("/", listController.createList);
+router.patch("/:id", listController.updateList);
+router.delete("/:id", listController.deleteList);
 
 
 
