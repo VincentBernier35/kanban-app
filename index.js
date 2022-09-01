3187f43a8227f8e882dec1917079c849d581adb6
VincentBernier35 <vincent.bernier@hotmail.fr>
2022-09-01 13:37:29 +0200

    🚧 link router and add dotenv on index.js(root)

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
+const router = require("./app/routers");
 const app = express();
 
 
 app.get("/", function(req, res) {
-  res.send("Hello World!");
+  res.send("Bienvenue sur kanban-app!");
 });
+app.use("/api", router);
 
 // start the app
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
