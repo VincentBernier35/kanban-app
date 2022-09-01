6356f34a110bbb51f8a48eb657a667ccafb075ff
VincentBernier35 <vincent.bernier@hotmail.fr>
2022-09-01 13:41:22 +0200

    💄 add color for server link on terminal

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.use("/api", router);
 // start the app
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+  console.log(`\x1b[1;33m\u26a1Running server on : http://localhost:${port} \u26a1\x1b[0m`);
 });
