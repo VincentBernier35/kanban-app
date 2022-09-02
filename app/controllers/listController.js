a618ab03e405da623e50b8d1171671b2206020e4
VincentBernier35 <vincent.bernier@hotmail.fr>
2022-09-02 16:09:14 +0200

    🚧 list controller in progress

--- app/controllers/listController.js
+++ app/controllers/listController.js
@@ -1,21 +1,61 @@
-// const { List } = require("../models");
+const sanitizeHtml = require("sanitize-html");
+const { List, Card } = require("../models");
 
-function getAllLists(req, res) {
-  res.send("Bienvenue dans getAllLists");
+//**************************************** */
+async function getAllLists(req, res) {
+  const lists = await List.findAll({
+    order: [
+      ["position", "ASC"],
+      ["created_at", "DESC"],
+      [ { model: Card, as: "cards" }, "position", "ASC"]
+    ], // attention cette technique alourdi la taille de la réponse, donc du transfert, donc la vitesse de réponses de notre API
+    include: {
+      association: "cards",
+      include: "tags",
+    } // attention cette technique alourdi la taille de la réponse, donc du transfert, donc la vitesse de réponses de notre API
+  });
+  res.json(lists); // je renvoie du json au client. Le statut est 200 par défaut. on peut le préciser ou non
 }
 
-function getOneList(req, res) {
-  res.send("bienvenue dans getOneList");
+//**************************************** */
+async function getOneList(req, res) {
+  const listId = parseInt(req.params.id);
+
+  if ( isNaN(listId) ) { // je vérifie le user input avant car si ce n'est pas bon cela épargne un appel à l'API
+    res.status(404).json({ error: "List not found please verify the provided id"});
+    return;
+  }
+  const list = await List.findByPk(listId); 
+  if (! list) { // si elle n'existe pas alors 404
+    res.status(404).json({ error: "List not found. Please verify the provided id."});
+    return; // je stoppe la fonction
+  }
+  res.json(list);
+}
+//**************************************** */
+async function createList(req, res) {
+  const { name, position } = req.body;
+
+  if (position && isNaN(position)) { // je protège l'API avec des early return
+    return res.status(400).json({ error: "Invalid type. position should return a number"});
+  }
+  if (! name) {
+    return res.status(400).json({ error: "Missing body parameter: name"});
+  }
+
+  const list = await List.create({
+    name: sanitizeHtml(name),
+    position
+  });
+
+  res.status(201).json(list);
 }
 
-function createList(req, res) {
-  console.log("Hello from createList");
-}
+//**************************************** */
+async function 
 
-function updateList(req, res) {
-  console.log("Hello from updateList");
-}
 
+//**************************************** */
 function deleteList(req, res) {
   console.log("Hello from deleteList");
 }
