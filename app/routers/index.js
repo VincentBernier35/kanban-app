223d352e360f5a6cb1235fdfd0a1a87d411ab55b
VincentBernier35 <vincent.bernier@hotmail.fr>
2022-09-03 18:08:33 +0200

    🚧 start the documentation with swagger

--- app/routers/index.js
+++ app/routers/index.js
@@ -3,11 +3,18 @@ const listRouter = require("./listRouter");
 const cardRouter = require("./cardRouter");
 const tagsRouter = require("./tagRouter");
 
+// documentation
+const swaggerUi = require("swagger-ui-express");
+const swaggerDoc = require("./swagger.json");
+
 const mainRouter = new Router(); // le but est de séparer chaque router dans un fichiers différent
 
+
 // routes already have "api" prefix
+mainRouter.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc)); // Setup the documentation
+
 mainRouter.use("/lists",listRouter);
-mainRouter.use("/cards",cardRouter); // TODO : find why the display of card Router isn't blue azul ?
+mainRouter.use("/cards",cardRouter);
 mainRouter.use("/tags", tagsRouter);
 
 module.exports = mainRouter;
