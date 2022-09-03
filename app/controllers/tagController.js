5d03900c646cfa5b84aa82ee9e6f51682a12a7f2
VincentBernier35 <vincent.bernier@hotmail.fr>
2022-09-03 11:47:25 +0200

    🚧 tag controller in progress

--- app/controllers/tagController.js
+++ app/controllers/tagController.js
@@ -1,34 +1,90 @@
+const { Tag, Card } = require("../models");
+const { isValidHexadecimalColor } = require("./utils");
 
-
-function getAllTags(req, res) {
-  res.send("Bienvenue dans getAllTags");
+//************************************** */
+async function getAllTags(req, res) {
+  // fetch from database
+  const tags = await Tag.findAll();
+  res.json(tags);
 }
 
-function getOneTag(req, res) {
-  res.send("bienvenue dans getOneTag");
+//************************************** */
+async function getOneTag(req, res) {
+  // collect the params id
+  const tagId = req.params.id;
+  // fetch from database
+  const tag = await Tag.findByPk(tagId);
+
+  if (! tag) {
+    return res.status(400).json({ error: "Tag not found. Please verify the provided id." });
+  }
+
+  res.json(tag);
 }
 
-function createTag(req, res) {
-  console.log("Hello from createTag");
+//************************************** */
+async function createTag(req, res) {
+  const { name, color } = req.body;
+
+  if (! name) {
+    return res.json({ error: "Missing (or empty) body parameter: 'name'."});
+  }
+  if (color && ! isValidHexadecimalColor(color)) {
+    return res.status(400).json({ error : "Invalid type: 'color' should be a hexadecimal code."});
+  }
+  const tag = await Tag.create({ name, color });
+  res.status(201).json(tag);
 }
 
-function updateTag(req, res) {
-  console.log("Hello from updateTag");
+//************************************** */
+async function updateTag(req, res) {
+  const tagId = req.params.id;
+  const { name, color } = req.body;
+
+  if (! name && ! color) {
+    return res.status(400).json({ error: "Invalid body. Should provide at least a 'name' or 'color' property."});
+  }
+  if (color && ! isValidHexadecimalColor(color)) {
+    return res.status(400).json({ error: "Invalid type: color should be a hexadecimal color (string)"});
+  }
+
+  const tag = await Tag.findByPk(tagId);
+  if (! tag) {
+    return res.status(400).json({ error: "Tag not found..."});
+  }
+
+  tag.name = name || tag.name;
+  tag.color = color || tag.color;
+  await tag.save();
+
+  res.send(tag);
 }
 
-function deleteTag(req, res) {
-  console.log("Hello from deleteTag");
+//************************************** */
+async function deleteTag(req, res) {
+  const tagId = req.params.id;
+
+  const tag = await Tag.findByPk(tagId);
+  if (! tag) {
+    return res.status(400).json( {error: "Tag not found..."});
+  }
+
+  await tag.destroy();
+
+  res.status(204).end;
 }
 
+//************************************** */
 function addTagToCard(req, res) {
-  res.send("Hello from addTagToCard");
+  
 }
 
+//************************************** */
 function removeTagFromCard(req, res) {
   res.send("hello from removeTagFromCard");
 }
 
-
+//************************************** */
 module.exports = {
   getAllTags,
   getOneTag,
