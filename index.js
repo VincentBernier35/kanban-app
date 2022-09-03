78e637406f9c79b73f57513f82b3c0ba858af067
VincentBernier35 <vincent.bernier@hotmail.fr>
2022-09-03 17:37:29 +0200

    🔒️ add Express rate limit. Use to limit repeated requests to public APIs and/or endpoints

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 require("dotenv").config();
 
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 const cors =require("cors");
 const router = require("./app/routers");
 const app = express();
+
+// Add rate limit policy
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 
 
 app.get("/", function(req, res) {
